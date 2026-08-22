cf2fd913194096e55fafc90a60f9b1cc3a9cf2e0
Prajin19 <yelloveforever197@gmail.com>
2026-08-22 14:54:16 +0530

    Changed db file

--- server/src/db.js
+++ server/src/db.js
@@ -1,5 +1,5 @@
 import 'dotenv/config'
-import { PrismaClient } from './generated/prisma'
+import { PrismaClient } from './generated/prisma/client.js'
 import { PrismaNeon } from '@prisma/adapter-neon'
 
 const neon = new PrismaNeon({
