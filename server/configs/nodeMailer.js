554fdb76af6a352262e13fe193263827f7f0d708
Prajin19 <yelloveforever197@gmail.com>
2026-08-23 11:24:24 +0530

    Fix duplicate Nodemailer declaration

--- server/configs/nodeMailer.js
+++ server/configs/nodeMailer.js
@@ -1,6 +1,6 @@
 import nodemailer from 'nodemailer';
 
-const nodemailer = require("nodemailer");
+//const nodemailer = require("nodemailer");
 
 // Create a transporter using SMTP
 const transporter = nodemailer.createTransport({
